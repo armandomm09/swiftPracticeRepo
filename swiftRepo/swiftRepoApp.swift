5f911656b8926543404720be5cc4298a2986e9d8
armandomm09 <pabloarmando.macbeath@gmail.com>
2023-12-30 00:20:40 -0600

    Initial Commit

---
+++ swiftRepo/swiftRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftRepoApp.swift
+//  swiftRepo
+//
+//  Created by Pablo Armando Mac Beath Milián on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
