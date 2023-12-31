d1999b10caedc4870b702781016ed897387124be
armandomm09 <pabloarmando.macbeath@gmail.com>
2023-12-31 03:45:22 -0600

    Geometry Reader

---
+++ swiftRepo/geometryReadeScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  geometryReadeScrollView.swift
+//  swiftRepo
+//
+//  Created by Pablo Armando Mac Beath Milián on 31/12/23.
+//
+
+import SwiftUI
+
+struct geometryReadeScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    geometryReadeScrollView()
+}
---
+++ swiftRepo/geometryReaderStacks.swift
@@ -0,0 +1,18 @@
+//
+//  geometryReaderStacks.swift
+//  swiftRepo
+//
+//  Created by Pablo Armando Mac Beath Milián on 31/12/23.
+//
+
+import SwiftUI
+
+struct geometryReaderStacks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    geometryReaderStacks()
+}
